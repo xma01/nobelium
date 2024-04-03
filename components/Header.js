4597e0241fc129a83c7affbc292d8da147e43d38
xma <144253193+xma01@users.noreply.github.com>
2024-04-03 22:53:15 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,10 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+  //{ id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },  //
+//{ id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },  //
+//  { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },   //
+// { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true    //
   ]
   return (
     <div className="flex-shrink-0">
